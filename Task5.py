432fcb9318997f617afd25e664819218eb9ed159
«VendyVL» <vedmansk@yandex.ru>
2022-09-05 00:54:59 +0300

    сортировка всё равно не работает2

--- Task5.py
+++ Task5.py
@@ -3,16 +3,17 @@
 list = ['green', 'blue', 'red', 'broun', 'orange', 'purple']
 
 list2 = []
-minimal = len(list[0])
-imin = 0
-while len(list2) <= len(list):
+a = len(list)
+while len(list2) <= a :
+    minimal = len(list[0]) # Как 0 может быть out of range?
+    imin = 0
     for i in range (0, len(list)):
-        if len(list[i]) < minimal and list[i]!="0": # Я не понимаю почему оно не выполняет условие неравенства
+        if len(list[i]) < minimal: 
             minimal = len(list[i])
             imin = i
         else: i+1 
     list2.append(list[imin])
-    list[imin] = "0"
+    list.pop(imin)
 
 print(list)
 print(list[imin])
